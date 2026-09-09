ce9efdb55ae49ef4c1b88ffbb7e569e9b42b11cd
sreynit722 <pochsreynich017@gmail.com>
2026-09-09 13:14:47 +0700

    update for phone

--- sw.js
+++ sw.js
@@ -1,22 +1,5 @@
-const CACHE = "pomodoro-v3";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
-});
+const CACHE="pomodoro-v4";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
